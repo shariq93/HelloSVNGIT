d2ccd73263fb370fee7966b9799ef6b3054c3809
Shariq <shariqsb93@gmail.com>
2017-03-24 13:09:12 +0500

    chenge to relative layout

--- app/src/main/java/comshariq/dell/hellosvngit/MainActivity.java
+++ app/src/main/java/comshariq/dell/hellosvngit/MainActivity.java
@@ -1,13 +1,16 @@
 package comshariq.dell.hellosvngit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+
     }
 }
