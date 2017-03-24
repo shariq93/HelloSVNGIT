94e7a78036b935d153e3071de137bb250dec8627
Shariq <shariqsb93@gmail.com>
2017-03-24 13:14:39 +0500

    chenge to relative layout

--- app/src/main/java/comshariq/dell/hellosvngit/MainActivity.java
+++ app/src/main/java/comshariq/dell/hellosvngit/MainActivity.java
@@ -1,13 +1,23 @@
 package comshariq.dell.hellosvngit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Short Click", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
