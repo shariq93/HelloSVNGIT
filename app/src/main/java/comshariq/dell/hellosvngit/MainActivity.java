3bf7e3bbf3ea9805ef01e3aaccb4a61d9d38a4af
Ibrahimarain <ibrahimarain15@gmail.com>
2017-03-24 13:16:27 +0500

    Hello Sir i am done with my work.

--- app/src/main/java/comshariq/dell/hellosvngit/MainActivity.java
+++ app/src/main/java/comshariq/dell/hellosvngit/MainActivity.java
@@ -13,10 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
+
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Short Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        tv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText(MainActivity.this, "Long Click", Toast.LENGTH_SHORT).show();
+
+                return false;
             }
         });
     }
